50de98d92ea4aa2c8700e225a3f9efc689a3a602
Angelina Filippova <linakirsanova@gmail.com>
2018-06-07 19:31:36 +0300

    Changed nickname display

--- app/services/slack_user_name_fetcher.rb
+++ app/services/slack_user_name_fetcher.rb
@@ -5,6 +5,7 @@ class SlackUserNameFetcher
 
   def self.call
     NomadCity.all.each do |record|
+      binding.pry
       fetch_nickname_from_slack(record) unless record.nickname
     end
   end
@@ -15,7 +16,7 @@ class SlackUserNameFetcher
     res = Net::HTTP.get_response(
       URI("#{API_URL}?token=#{ENV['NOMAD_SLACK_API_TOKEN']}&user=#{record.user}")
     )
-    nickname = JSON.parse(res.body, symbolize_names: true).dig(:user, :name)
+    nickname = JSON.parse(res.body, symbolize_names: true).dig(:user, :profile, :display_name)
 
     record.update(nickname: nickname)
   end
